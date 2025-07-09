9a3a447e54cc2f6db34527dff466ee7c4b9d749d
hori_n <hori.n@e-channel.co.jp>
2025-07-09 10:24:44 +0900

    githubpagesへのデプロイ対応

--- gal-site/astro.config.mjs
+++ gal-site/astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  base: '/gal-site',
+  site: 'https://st-man-hori.github.io/',
+  base: '/gal-site-web',
   integrations: [tailwind()],
   output: 'static',
-  outDir: './dist',
+  outDir: '../gal-site-web',
 });
